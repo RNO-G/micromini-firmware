a4100ec6e12d98907d54f7d1e16a2205805a9482
nilsphysics <nils.heyer@physics.uu.se>
2025-10-15 13:08:41 -0700

    no init when already initialized

--- client/init_log_power.sh
+++ client/init_log_power.sh
@@ -2,4 +2,9 @@
 
 #initialize power logging
 /home/rno-g/rno-g-BBB-scripts/gpio/select-ext-i2c.sh
-/home/rno-g/rno-g-BBB-scripts/w1/setup-w1
+
+if i2cdetect -y -r 2 | grep -q "UU"; then
+    echo "DS2482 already initialized"
+else
+    /home/rno-g/rno-g-BBB-scripts/w1/setup-w1 
+fi
